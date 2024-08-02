1de1ad8eb735ea2681a936400a5c527dff1c2dae
CaptainSP <19982002ke@gmail.com>
2024-08-02 16:45:07 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,164 @@
+const {
+  app,
+  BrowserWindow,
+  session,
+  Menu,
+  protocol,
+  ipcMain,
+} = require("electron");
+const os = require("os");
+
+let mainWindow;
+
+function getLocalIpAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const net of interfaces[name]) {
+      // IPv4 ve non-internal bir adres olup olmadığını kontrol etme
+      if (net.family === "IPv4" && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+  return "127.0.0.1"; // Yerel IP adresi bulunamazsa varsayılan olarak localhost döner
+}
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      nodeIntegrationInWorker: true,
+      session: true,
+      devTools: true,
+      webSecurity: false,
+      preload: __dirname + "/preload.js",
+    },
+    autoHideMenuBar: true,
+  });
+
+  mainWindow.loadURL("https://kiosk.cerebeye.io/");
+  //mainWindow.loadFile("index.html");
+
+  // İlk açıldığında tam ekran moduna geç
+  mainWindow.setKiosk(false);
+
+  mainWindow.webContents.openDevTools();
+
+  // Kamera iznini otomatik vermek için
+  session.defaultSession.setPermissionRequestHandler(
+    (webContents, permission, callback) => {
+      if (permission === "media") {
+        return callback(true); // Kamera ve mikrofon izinlerini otomatik olarak ver
+      }
+      callback(false);
+    }
+  );
+
+  session.defaultSession.setDisplayMediaRequestHandler((request, callback) => {
+    desktopCapturer.getSources({ types: ["screen", video] }).then((sources) => {
+      // Grant access to the first screen found.
+      callback({ video: sources[0], audio: "loopback" });
+    });
+  });
+
+  mainWindow.on("close", () => {
+    // Uygulamayı kapatmadan önce tam ekran modundan çıkmak
+    mainWindow.setKiosk(false);
+  });
+
+  // Menüyü oluştur ve uygula
+  createMenu();
+}
+
+function createMenu() {
+  const localIpAddress = getLocalIpAddress(); // IP adresini al
+  const targetUrl = `http://10.0.0.16:3001/login`; // Kullanılacak URL
+  console.log(targetUrl);
+  const template = [
+    {
+      label: "My Menu",
+      submenu: [
+        {
+          label: "Go to URL",
+          click() {
+            mainWindow.webContents.openDevTools();
+
+            mainWindow.loadURL(targetUrl);
+          },
+        },
+        // close app
+        {
+          label: "Close",
+          click() {
+            app.quit();
+          },
+        },
+      ],
+    },
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  //if (process.platform !== "darwin") {
+  app.quit();
+  //}
+});
+
+protocol.registerSchemesAsPrivileged([
+  {
+    scheme: "http",
+    privileges: {
+      bypassCSP: true,
+      secure: true,
+      supportFetchAPI: true,
+      corsEnabled: true,
+    },
+  },
+]);
+
+ipcMain.handle("print-iframe", async (event, iframeHTML, printName) => {
+  const printWindow = new BrowserWindow({ show: true });
+
+  printWindow.loadURL(`data:text/html;charset=utf-8,${encodeURI(iframeHTML)}`);
+
+  printWindow.webContents.on("did-finish-load", async () => {
+    /*const printerList = await printWindow.webContents.getPrintersAsync();
+    const defaultPrinter = printerList.find((printer) => printer.isDefault);
+
+    printWindow.webContents.print(
+      {
+        deviceName: printName || defaultPrinter.name,
+        silent: true,
+        printBackground: true,
+      },
+      () => {
+        printWindow.close();
+      }
+    );*/
+  });
+});
+
+ipcMain.handle('get-printers', async (event) => {
+  const focusedWindow = BrowserWindow.getFocusedWindow();
+  if (focusedWindow) {
+    const printers = await focusedWindow.webContents.getPrintersAsync();
+    return printers;
+  }
+  return [];
+});
---
+++ preload.js
@@ -0,0 +1,9 @@
+const { contextBridge, ipcRenderer, webContents } = require('electron');
+
+// Güvenli bir şekilde belirli node işlevlerini veya modüllerini pencere (window) nesnesine köprüleyin
+contextBridge.exposeInMainWorld('electron', {
+    ipcRenderer: ipcRenderer,
+    printIframe: (iframeContent, printName) => ipcRenderer.invoke('print-iframe', iframeContent,printName),
+    getPrinters: () => ipcRenderer.invoke('get-printers'),
+    // Diğer API'leri buraya ekleyebilirsiniz
+});
