fc9f36248c408422f9fcc1ced4bc26ac0b75faac
CaptainSP <19982002ke@gmail.com>
2024-08-16 16:01:57 +0300

    add

--- index.js
+++ index.js
@@ -171,6 +171,7 @@ recording = recorder.record({
   channels: 1, // Mono audio
   audioType: "wav", // Output audio type,
   recorder: "sox", // Try also "arecord" or "sox"
+  device: "plughw:1,0", // Try also "plughw:1,0" or "plughw:0,0"
 });
 audioStream = recording.stream();
 audioStream.on("end", () => {
