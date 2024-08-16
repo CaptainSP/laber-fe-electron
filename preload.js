1b0af63efb54b2ded9415088dbc2f8ddfc79f555
CaptainSP <19982002ke@gmail.com>
2024-08-16 13:08:21 +0300

    chore: Add initial project files and dependencies

---
+++ preload.js
@@ -0,0 +1,20 @@
+const { contextBridge, ipcRenderer, webContents } = require("electron");
+
+// Güvenli bir şekilde belirli node işlevlerini veya modüllerini pencere (window) nesnesine köprüleyin
+contextBridge.exposeInMainWorld("electron", {
+  ipcRenderer: ipcRenderer,
+  printIframe: (iframeContent, printName) =>
+    ipcRenderer.invoke("print-iframe", iframeContent, printName),
+  getPrinters: () => ipcRenderer.invoke("get-printers"),
+  saveVideo: (videoArrayBuffer) =>
+    ipcRenderer.invoke("save-video", videoArrayBuffer),
+  mergeVideo: (music) => ipcRenderer.invoke("merge-video", music),
+  startVideo: () => ipcRenderer.invoke("start-video"),
+  stopVideo: () => ipcRenderer.invoke("stop-video"),
+  deleteVideo: () => ipcRenderer.invoke("delete-video"),
+  saveImage: (imageArrayBuffer) =>
+    ipcRenderer.invoke("save-image", imageArrayBuffer),
+  getVideo: (path) => ipcRenderer.invoke("get-video", path),
+  startRecording: () => ipcRenderer.invoke("start-recording"),
+  // Diğer API'leri buraya ekleyebilirsiniz
+});
